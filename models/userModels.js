f1c749a94dd9c2d59d2ead887a7f6dc9b86b9656
mygithubshivam <kumarshivam3127@gmail.com>
2024-09-24 22:31:14 +0530

    added files

---
+++ models/userModels.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("users", userSchema);
